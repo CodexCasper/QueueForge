0bb8da535a3eb8dee196f7e172f2547897dc36ba
CodexCasper <parth.23mip10104@vitbhopal.ac.in>
2026-07-24 22:30:47 +0530

    clean up worker implementation

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 
 const app = require("./app");
+const { startWorker } = require("./worker/worker");
+
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT , () => {
+
     console.log(`the server is running on http://localhost:${PORT}`);
+
+    startWorker();
 });
