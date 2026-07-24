6eb187c245210a3311646a96ae1261efb676ef5f
CodexCasper <parth.23mip10104@vitbhopal.ac.in>
2026-07-24 22:29:48 +0530

    feat(worker): add retry handling for failed jobs

---
+++ server/src/worker/processor.js
@@ -0,0 +1,16 @@
+const processJob = async(job) => {
+
+    console.log(`Processing: ${job.jobName}...`);
+
+    await new Promise((resolve) => {
+        setTimeout(resolve , 3000);
+    })
+
+    
+    console.log("Job Finished");
+} 
+
+module.exports = {
+    processJob,
+
+}
