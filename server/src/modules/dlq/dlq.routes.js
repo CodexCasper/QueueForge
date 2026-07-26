49e5c3a8448cbb92a9f0793263fec92581973324
CodexCasper <parth.23mip10104@vitbhopal.ac.in>
2026-07-26 16:24:30 +0530

    Add replay job API route

--- server/src/modules/dlq/dlq.routes.js
+++ server/src/modules/dlq/dlq.routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { deleteFailedJob, getAllFailedJobs } = require("./dlq.controller");
+const { deleteFailedJob, getAllFailedJobs, replayJob } = require("./dlq.controller");
 
 const router = express.Router();
 
 router.get("/" , getAllFailedJobs);
 
 router.delete("/:id" , deleteFailedJob);
+
+router.post("/:id/replay" , replayJob);
+
 
 module.exports = router;
