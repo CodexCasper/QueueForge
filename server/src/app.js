43f70e475bf85b196afd64fc7f914df06428c573
CodexCasper <parth.23mip10104@vitbhopal.ac.in>
2026-07-26 15:39:46 +0530

    Register replay job API

--- server/src/app.js
+++ server/src/app.js
@@ -4,15 +4,19 @@ const cors = require("cors");
 const jobsRouter = require("./modules/jobs/jobs.routes");
 const errorHandler = require("./middleware/error.middleware")
 const dlqRoutes = require("./modules/dlq/dlq.routes");
+const workersRoutes = require("./modules/workers/workers.routes");
+
 
 const app = express();
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
-app.use("/api/jobs" , jobsRouter);
-ap.use("/api/dlq" , dlqRoutes);
 
+
+app.use("/api/jobs" , jobsRouter);
+app.use("/api/dlq" , dlqRoutes);
+app.use("/api/workers" , workersRoutes);
 
 app.get("/" , (req,res) => {
     console.log("API is running correctly");
