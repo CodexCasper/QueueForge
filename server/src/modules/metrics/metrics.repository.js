d085a072b096e76687f9be8e44ebd953860ed58d
CodexCasper <parth.23mip10104@vitbhopal.ac.in>
2026-07-26 16:54:15 +0530

    Group jobs by status for metrics

--- server/src/modules/metrics/metrics.repository.js
+++ server/src/modules/metrics/metrics.repository.js
@@ -3,7 +3,17 @@ const prisma = require("../../config/prisma");
 const getMetrics = async () => {
 
     const totalJobs = await primsa.job.count();
+
+    const jobsByStatus = await prisma.job.groupBy({
+
+        by:["status"],
+        _count: {
+            status: "true",
+        },
+    });
+
 }
+
 
 module.exports = {
     getMetrics,
