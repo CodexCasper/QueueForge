71a07a5d86f305f0423c19d20d61664dae52533e
CodexCasper <parth.23mip10104@vitbhopal.ac.in>
2026-07-28 11:30:56 +0530

    feat(jwt): completed centralized jwt authentication

---
+++ server/src/modules/auth/auth.repository.js
@@ -0,0 +1,75 @@
+const prisma = require("../../config/prisma");
+
+const create = async (userData) => {
+
+    return prisma.User.create({
+        data: userData,
+    });
+
+};
+
+const findByEmail = async (email) => {
+
+    return prisma.User.findUnique({
+        where: {
+            email,
+        },
+    });
+
+};
+
+const findById = async (id) => {
+    
+    return prisma.User.findUnique({
+        where: {
+            id,
+        },
+    });
+
+};
+
+const updateRefreshToken = async (id , refreshToken) => {
+    
+    return prisma.User.update({
+        where: {
+            id,
+        }, 
+        data: {
+            refreshToken
+        },
+    });
+
+};
+
+const updateProfile = async (id , data) => {
+    
+    return prisma.User.update({
+        where: {
+            id,
+        },
+        data,
+    });
+
+};
+
+const updatePassword = async (id , password) => {
+    
+    return prisma.User.update({
+        where: {
+            id,
+        },
+        data: {
+            password
+        },
+    });
+
+};
+
+
+module.exports = {
+    create,
+    findByEmail,
+    findById,
+    updatePassword,
+    updateProfile
+}
